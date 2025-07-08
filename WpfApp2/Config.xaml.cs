aac84555bb1f26b2b8c83412358749e3b971d768
lummer <lummer@WS-162DBB>
2025-07-08 11:44:07 +0300

    bvg

--- WpfApp2/Config.xaml.cs
+++ WpfApp2/Config.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace WpfApp2
-{
+{mm
     /// <summary>
     /// Логика взаимодействия для Config.xaml
     /// </summary>
@@ -23,5 +25,53 @@ namespace WpfApp2
         {
             InitializeComponent();
         }
+
+        public class TextBoxWriter
+        {
+            public static void WriteToFile(TextBox[] textBoxes, string filePath)
+            {
+                // Проверка входных данных
+                if (textBoxes == null || textBoxes.Length == 0)
+                {
+                    MessageBox.Show("Нет текстовых полей для записи");
+                    return;
+                }
+
+                try
+                {
+                    // Создаем или перезаписываем файл
+                    using (StreamWriter writer = new StreamWriter(filePath, true, Encoding.UTF8))
+                    {
+                        foreach (TextBox textBox in textBoxes)
+                        {
+                            // Записываем содержимое каждого TextBox
+                            writer.Write(textBox.Text);
+                            writer.Write(";");
+                        }
+                        writer.Write("\n");
+                    }
+
+                    MessageBox.Show("Данные успешно записаны в файл!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка записи: {ex.Message}");
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            // Получаем все нужные TextBox
+            TextBox[] inputBoxes = { NameEntry, IdEntry, Byte };
+
+            // Указываем путь к файлу
+            string filePath = @"C:\data\config.txt";
+
+            // Вызываем функцию записи
+            TextBoxWriter.WriteToFile(inputBoxes, filePath);
+        }
+
+
     }
 }
