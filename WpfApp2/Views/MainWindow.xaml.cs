512a47afda0451eb5583011128e7334d859bc044
your-name <daniil.zamyshlyaev@main.ru>
2025-07-09 11:50:25 +0300

    только окно

--- WpfApp2/Views/MainWindow.xaml.cs
+++ WpfApp2/Views/MainWindow.xaml.cs
@@ -8,9 +8,14 @@ namespace WpfApp2.Views
         {
             InitializeComponent();
         }
-        private void Button_New(object sender, RoutedEventArgs e)
+        private void Button_Config(object sender, RoutedEventArgs e)
         {
             ConfigWindow taskWindow = new ConfigWindow();
+            taskWindow.Show();
+        }
+        private void Button_Excel(object sender, RoutedEventArgs e)
+        {
+            ExcelWindow taskWindow = new ExcelWindow();
             taskWindow.Show();
         }
     }
